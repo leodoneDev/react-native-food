aea4e00b35e3bf906f3904cbc0065b0f597da6b3
leodoneDev <leodone125@gmail.com>
2025-04-10 00:55:11 +0700

    config setting

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -10,7 +10,7 @@ export default function BackButton(props) {
     </TouchableHighlight>
   );
 }
-123123123123123123123
+12312312312312312
 BackButton.propTypes = {
   onPress: PropTypes.func,
   source: PropTypes.number,
